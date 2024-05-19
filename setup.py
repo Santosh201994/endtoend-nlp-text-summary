a67c97b5ff93d1e63ccb3a20103922d74404a716
Santosh201994 <161206769+Santosh201994@users.noreply.github.com>
2024-05-19 18:30:48 +0000

    update

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "endtoend-nlp-text-summary"
+AUTHOR_USER_NAME = "Santosh201994"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "santosh201994@gmail.com"
 
 
 
